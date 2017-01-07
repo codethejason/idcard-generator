fafefc0d1a68a0177f66eb448e65eef1a05e048e
codethejason <codethejason@outlook.com>
2017-01-07 18:48:11 -0500

    Changed page to flex and can now print more than 6 cards

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# idcard-generator
+# ID Card Generator
 Generates a set of ID cards to print out for hackathons based on given parameters.
+
+Only works with Chrome.
--- script.js
+++ script.js
@@ -4,8 +4,11 @@ var users = [
   {name: "Jacobian Georgian", role: "hacker", id: 3},
   {name: "Jacob Georgian", role: "hacker", id: 4},
   {name: "Jason Georgian", role: "hacker", id: 5},
-  {name: "George Georgian", role: "hacker", id: 6}
+  {name: "George Georgian", role: "hacker", id: 6},
+  {name: "Justin Georgian", role: "hacker", id: 7}
 ];
+
+//maybe add group id (id in hacker or mentors group)
 
 window.onload = function() {
   //JsBarcode("#hacker1barcode", "1234567890123", {format: "itf14", width: 1.4, height: 40, fontSize: 16});
@@ -16,17 +19,24 @@ window.onload = function() {
     var backTemplate = document.querySelector('#cardbackTemplate');
     var cardsPerPage = 6;
     var userCounter = 0;
-    for(var i = 0; i < Math.floor(users.length/cardsPerPage)*2; i++) { //loop through pages, each user takes two pages
+    var lastpage = Math.ceil(users.length/cardsPerPage)*2-1;
+    for(var i = 0; i < lastpage+1; i++) { //loop through pages, each user takes two pages
       var currentPage = document.createElement("DIV"); //new page
       pagesContainer.appendChild(currentPage);
       currentPage.className = "page";
       var pageNum = i;
+      if(i == lastpage-1 && users.length%cardsPerPage != 0) { //if it's the last page and it's not full (last page as in last set)
+        cardsPerPage = users.length%cardsPerPage; //remaining users
+        console.log(cardsPerPage);
+      } else {
+        console.log(i);
+        console.log(lastpage);
+      }
       if(pageNum%2 == 0) { //if it's an odd page number (index starts at 0)
         for(var j = 0; j < cardsPerPage; j++) {
           var newEl = frontTemplate.content.cloneNode(true);
           var participantName = newEl.querySelector(".participant");
           var role = newEl.querySelector(".role");
-          console.log(j);
           participantName.textContent = users[userCounter].name;
           role.textContent = users[userCounter].role.toUpperCase();
           currentPage.appendChild(newEl);
@@ -37,10 +47,10 @@ window.onload = function() {
         for(var j = 0; j < cardsPerPage; j++) {
           var newEl = backTemplate.content.cloneNode(true);
           var barcode = newEl.querySelector("svg");
-          barcode.id = "hacker1";
+          barcode.id = users[userCounter].role + users[userCounter].id;
           //barcodeID.id = userCounter;
           currentPage.appendChild(newEl);
-          JsBarcode("#hacker1", "1000000000001", {format: "itf14", width: 1.4, height: 40, fontSize: 16});
+          JsBarcode("#"+barcode.id, "100000000000"+users[userCounter].id, {format: "itf14", width: 1.4, height: 40, fontSize: 16});
           userCounter++;
         }
       }
