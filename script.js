c5014e98e109b4708c38a4523871889db83a3c76
codethejason <codethejason@outlook.com>
2017-01-08 17:46:07 -0500

    working submit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var users = [
+var users = [];
+
+let sampleData = [
   {name: "John Georgian", role: "hacker", id: 1},
   {name: "Jake Georgian", role: "hacker", id: 2},
   {name: "Jacobian Georgian", role: "hacker", id: 3},
@@ -8,77 +10,76 @@ var users = [
   {name: "Justin Georgian", role: "hacker", id: 7}
 ];
 
-window.onload = function() {
-  //JsBarcode("#hacker1barcode", "1234567890123", {format: "itf14", width: 1.4, height: 40, fontSize: 16});
 
-  function generateCards(users) {
-    var pagesContainer = document.querySelector('.pages');
-    var frontTemplate = document.querySelector('#cardfrontTemplate');
-    var backTemplate = document.querySelector('#cardbackTemplate');
-    var cardsPerPage = 6;
-    var userCounter = 0;
-    var lastpage = Math.ceil(users.length/cardsPerPage)*2-1;
-    for(var i = 0; i < lastpage+1; i++) { //loop through pages, each user takes two pages
-      var currentPage = document.createElement("DIV"); //new page
-      pagesContainer.appendChild(currentPage);
-      currentPage.className = "page";
-      var pageNum = i;
-      if(i == lastpage-1 && users.length%cardsPerPage != 0) { //if it's the last page and it's not full (last page as in last set)
-        cardsPerPage = users.length%cardsPerPage; //remaining users
-        console.log(cardsPerPage);
+function generateCards(users) {
+  var pagesContainer = document.querySelector('.pages');
+  var frontTemplate = document.querySelector('#cardfrontTemplate');
+  var backTemplate = document.querySelector('#cardbackTemplate');
+  var cardsPerPage = 6;
+  var userCounter = 0;
+  var lastpage = Math.ceil(users.length/cardsPerPage)*2-1;
+  for(var i = 0; i < lastpage+1; i++) { //loop through pages, each user takes two pages
+    var currentPage = document.createElement("DIV"); //new page
+    pagesContainer.appendChild(currentPage);
+    currentPage.className = "page";
+    var pageNum = i;
+    if(i == lastpage-1 && users.length%cardsPerPage != 0) { //if it's the last page and it's not full (last page as in last set)
+      cardsPerPage = users.length%cardsPerPage; //remaining users
+      console.log(cardsPerPage);
+    }
+    if(pageNum%2 == 0) { //if it's an odd page number (index starts at 0)
+      for(var j = 0; j < cardsPerPage; j++) {
+        var newEl = frontTemplate.content.cloneNode(true);
+        var participantName = newEl.querySelector(".participant");
+        var role = newEl.querySelector(".role");
+        participantName.textContent = users[userCounter].name;
+        role.textContent = users[userCounter].role.toUpperCase();
+        currentPage.appendChild(newEl);
+        userCounter++;
       }
-      if(pageNum%2 == 0) { //if it's an odd page number (index starts at 0)
-        for(var j = 0; j < cardsPerPage; j++) {
-          var newEl = frontTemplate.content.cloneNode(true);
-          var participantName = newEl.querySelector(".participant");
-          var role = newEl.querySelector(".role");
-          participantName.textContent = users[userCounter].name;
-          role.textContent = users[userCounter].role.toUpperCase();
-          currentPage.appendChild(newEl);
-          userCounter++;
+    } else {
+      userCounter -= cardsPerPage; //go back and do the back of the cards
+      for(var j = 0; j < cardsPerPage; j++) {
+        var newEl = backTemplate.content.cloneNode(true);
+        var barcode = newEl.querySelector("svg");
+        barcode.id = users[userCounter].role + users[userCounter].id;
+        //barcodeID.id = userCounter;
+        currentPage.appendChild(newEl);
+        var head, tail;
+        switch(users[userCounter].role.toLowerCase()) {
+          case "hacker": head = "10"; break;
+          case "organizer": head = "20"; break;
+          case "sponsor": head = "30"; break;
+          case "mentor": head = "40"; break;
+          case "volunteer": head = "50"; break;
+          case "special": head = "60"; break;
+          default: head = "70";
         }
-      } else {
-        userCounter -= cardsPerPage; //go back and do the back of the cards
-        for(var j = 0; j < cardsPerPage; j++) {
-          var newEl = backTemplate.content.cloneNode(true);
-          var barcode = newEl.querySelector("svg");
-          barcode.id = users[userCounter].role + users[userCounter].id;
-          //barcodeID.id = userCounter;
-          currentPage.appendChild(newEl);
-          var head, tail;
-          switch(users[userCounter].role) {
-            case "hacker": head = "10"; break;
-            case "organizer": head = "20"; break;
-            case "sponsor": head = "30"; break;
-            case "mentor": head = "40"; break;
-            case "volunteer": head = "50"; break;
-            case "special": head = "60"; break;
-            default: head = "70";
-          }
-          switch(users[userCounter].id.toString().length) { //check number of digits of ID
-            case 1: tail = "000".concat(users[userCounter].id.toString()); break;
-            case 2: tail = "00".concat(users[userCounter].id.toString()); break;
-            case 3: tail = "0".concat(users[userCounter].id.toString()); break;
-            default: tail = users[userCounter].id.toString();
-          } 
-          
-          JsBarcode("#"+barcode.id, head.concat(tail), {format: "CODE128", width: 2.2, height: 40, fontSize: 16});
-          userCounter++;
-        }
+        switch(users[userCounter].id.toString().length) { //check number of digits of ID
+          case 1: tail = "000".concat(users[userCounter].id.toString()); break;
+          case 2: tail = "00".concat(users[userCounter].id.toString()); break;
+          case 3: tail = "0".concat(users[userCounter].id.toString()); break;
+          default: tail = users[userCounter].id.toString();
+        } 
+
+        JsBarcode("#"+barcode.id, head.concat(tail), {format: "CODE128", width: 2.2, height: 40, fontSize: 16});
+        userCounter++;
       }
     }
   }
-  
-  generateCards(users);
 }
+  
+
 
 function converttoReadable() {
   var ids = document.querySelectorAll('.rowID');
   var textarea = document.querySelector('.jsonInput');
   var tbody = document.querySelector('#infoTable tbody');
+  if(!textarea.value) {
+    return;
+  }
   var jsonData = JSON.parse(textarea.value);
   tbody.textContent = "";
-  console.log(jsonData.length);
   for (var i=0; i < jsonData.length; i++) {
     var row = addRow();
     var id = document.querySelectorAll('.rowID')[i];
@@ -110,6 +111,14 @@ function converttoJSON() {
 //loads data into the users array
 function submitJSON() {
   var dialog = document.querySelector('.dialog').style.display = "none";
+  var textarea = document.querySelector('.jsonInput');
+  if(textarea.value) {
+    users = JSON.parse(textarea.value);
+  } else {
+    converttoJSON();
+    users = JSON.parse(textarea.value);
+  }
+  generateCards(users);
 }
 
 function addRow() {
